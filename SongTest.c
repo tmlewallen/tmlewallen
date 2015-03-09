0e21f8e2c71b6ce2a975b1fce167f519d0b755f1
tmlewalen <tmlewallen@crimson.ua.edu>
2015-03-09 15:54:35 -0500

    restoring backup

--- SongTest.c
+++ SongTest.c
@@ -32,28 +32,79 @@ main()
     setTempo(175);
     setTime(4,4);
     setStride(0.02);
-    setSustain(0.99998);
+    setSustain(0.99995);
     setAmplitude(0.3);
 
     openOutput("SongTest.rra",0,0);
 
 
-    // b(1,Q,instrument,octave,"-x","--x",SX);
-    // b(6,I,instrument,octave,"x",SX);
-    // // ci1(1,W,instrument,octave);
-    // b(5,H,instrument,octave,"xsS","xsS","xsS","xsS",SX);
-    // b(5,H,instrument,octave,"xsS","xsS","xsS","xsS",SX);
-    // b(5,H,instrument,octave,"xsS","xUS","xuS","xsS",SX);
-    // b(5,Q,instrument,octave,"usS","SuS",SX);
-    // b(5,Q,instrument,octave,"xsS",SX);
-    // ci1(1,I,instrument,octave);
-    // ci1(1,I,instrument,octave);
-    // ci1(1,I,instrument,octave);
-    // ci1(1,I,instrument,octave);
-    // ci1(1,I,instrument,octave);
-    // ci1(1,I,instrument,octave);
-    // ci1(1,I,instrument,octave);
-    // ci1(1,Qd,instrument,octave);
+    b(1,Q,instrument,octave,"-x","--x",SX);
+    b(6,I,instrument,octave,"x",SX);
+    // ci1(1,W,instrument,octave);
+    b(5,H,instrument,octave,"xsS","xsS","xsS","xsS",SX);
+    b(5,H,instrument,octave,"xsS","xsS","xsS","xsS",SX);
+    b(5,H,instrument,octave,"xsS","xUS","xuS","xsS",SX);
+    b(5,Q,instrument,octave,"usS","SuS",SX);
+    b(5,Q,instrument,octave,"xsS",SX);
+    ci1(1,I,instrument,octave);
+    ci1(1,I,instrument,octave);
+    ci1(1,I,instrument,octave);
+    ci1(1,I,instrument,octave);
+    ci1(1,I,instrument,octave);
+    ci1(1,I,instrument,octave);
+    ci1(1,I,instrument,octave);
+    ci1(1,I,instrument,octave);
+    cchord(1,I,instrument,octave,"x--s");
+    cchord(1,I,instrument,octave,"x--s");
+    // cchord(1,I,instrument,octave,"s--S");
+    // cchord(1,I,instrument,octave,"s--S");
+    cchord(2,I,instrument,octave,"x--S");
+    cchord(2,I,instrument,octave,"x--S");
+    cchord(2,I,instrument,octave,"s--u");
+    cchord(2,I,instrument,octave,"s--u");
+    cchord(3,I,instrument,octave,"x--S");
+    b(4,Q,instrument,octave-1,"x-xs",SX);
+    b(4,I,instrument,octave-1,"x",SX);
+    b(4,H,instrument,octave-1,"u","U","--x","--x",SX);
+    b(4,H,instrument,octave-1,"--S","--x","---s","---s",SX);
+    b(4,H,instrument,octave-1,"--u","--S","--x","--x",SX);
+    b(4,I,instrument,octave-1,"U",SX);
+    b(1,Q,instrument,octave-1,"x",SX);
+
+    rest(I);
+    b(1,H,instrument,octave,"xx-b","-","xx-b","xx-b",SX);
+    rest(Q);
+    b(1,H,instrument,octave,"xx-b","-","xx-b","xx-b",SX);
+    rest(I);
+
+    b(1,I,instrument,octave-1,"U",SX);
+    b(4,I,instrument,octave-1,"x",SX);
+    b(5,Q,instrument,octave-1,"x-xS",SX);
+    b(5,I,instrument,octave-1,"x",SX);
+    b(5,H,instrument,octave-1,"u","U","--x","--x",SX);
+    b(5,H,instrument,octave-1,"--S","--x","---S","---S",SX);
+    b(5,H,instrument,octave-1,"--u","--S","--x","--x",SX);
+    b(5,I,instrument,octave-1,"U",SX);
+    b(4,Q,instrument,octave-1,"x-xs",SX);
+    b(4,I,instrument,octave-1,"x",SX);
+    b(4,H,instrument,octave-1,"u","U","--x","--x",SX);
+    b(4,H,instrument,octave-1,"--S","--x","--u","--S",SX);
+    b(4,H,instrument,octave-1,"--x","-s","U","S",SX);
+    b(1,I,instrument,octave-1,"U",SX);
+    b(1,Q,instrument,octave-1,"x-xs",SX);
+    b(1,I,instrument,octave-1,"x",SX);
+    b(1,H,instrument,octave-1,"u","U","--x","--x",SX);
+    b(1,H,instrument,octave-1,"--S","--x","---s","---s",SX);
+    b(1,H,instrument,octave-1,"--u","--S","--x","--x",SX);
+    b(1,I,instrument,octave-1,"U",SX);
+    b(1,Q,instrument,octave-1,"x",SX);
+    rest(I);
+    b(1,H,instrument,octave,"xx-b","-","xx-b","xx-b",SX);
+    rest(Q);
+    b(1,I,instrument,octave,"xx-b",SX);
+    b(5,Q,instrument,octave-1,"B","b",SX);
+    b(5,Q,instrument,octave-1,"xx-x",SX);
+    b(5,Q,instrument,octave-1,"xx-x","xx-x",SX);
 VERSE:
 
     // 1: 1-5-8 "x-xs"
